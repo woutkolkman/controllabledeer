0701ca10074d3abd7c3ac2998d1da47e941e57f8
woutkolkman <wout.kolkman1@hotmail.com>
2023-07-26 20:29:52 +0200

    init

---
+++ ControllableDeer/Hooks.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace ControllableDeer
+{
+    class Hooks
+    {
+        public static void Apply()
+        {
+            //at tickrate
+            On.RainWorldGame.Update += RainWorldGameUpdateHook;
+
+            //at framerate
+            On.RainWorldGame.RawUpdate += RainWorldGameRawUpdateHook;
+        }
+
+
+        public static void Unapply()
+        {
+            //TODO
+        }
+
+
+        //at tickrate
+        static void RainWorldGameUpdateHook(On.RainWorldGame.orig_Update orig, RainWorldGame self)
+        {
+            orig(self);
+
+            if (self?.world?.activeRooms?.Count <= 0)
+                return;
+
+            foreach (Room room in self.world.activeRooms)
+            {
+                for (int i = 0; i < room?.physicalObjects?.Length; i++)
+                {
+                    for (int j = 0; j < room.physicalObjects[i].Count; j++)
+                    {
+                        PhysicalObject obj = room.physicalObjects[i][j];
+                        if (!(obj is Deer))
+                            continue;
+                    }
+                }
+            }
+        }
+
+
+        //at framerate
+        static void RainWorldGameRawUpdateHook(On.RainWorldGame.orig_RawUpdate orig, RainWorldGame self, float dt)
+        {
+            orig(self, dt);
+            if (self.GamePaused || self.pauseUpdate || !self.processActive || self.pauseMenu != null)
+                return;
+        }
+    }
+}
---
+++ ControllableDeer/Plugin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using System.Security.Permissions;
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace ControllableDeer
+{
+    //also edit version in "modinfo.json"
+    [BepInPlugin("maxi-mol.controllabledeer", "Controllable Deer", "0.1.0")] //(GUID, mod name, mod version)
+    public class Plugin : BaseUnityPlugin
+    {
+        public static new ManualLogSource Logger { get; private set; } = null;
+
+        //reference metadata
+        public static string GUID;
+        public static string Name;
+        public static string Version;
+
+        private static bool IsEnabled = false;
+
+
+        //called when mod is loaded, subscribe functions to methods of the game
+        public void OnEnable()
+        {
+            if (IsEnabled) return;
+            IsEnabled = true;
+
+            Logger = base.Logger;
+            Hooks.Apply();
+            Patches.Apply();
+
+            GUID = Info.Metadata.GUID;
+            Name = Info.Metadata.Name;
+            Version = Info.Metadata.Version.ToString();
+
+            Plugin.Logger.LogInfo("OnEnable called");
+        }
+
+
+        //called when mod is unloaded
+        public void OnDisable()
+        {
+            if (!IsEnabled) return;
+            IsEnabled = false;
+
+            Hooks.Unapply();
+            Patches.Unapply();
+
+            Plugin.Logger.LogInfo("OnDisable called");
+        }
+    }
+}
